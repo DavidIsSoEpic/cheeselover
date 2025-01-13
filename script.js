f6831b2e10ba67fa3e8c67808d5bb9a4b2ff5d36
dogpee <157509341+DavidIsSoEpic@users.noreply.github.com>
2025-01-13 16:35:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function scrollToAbout() {
-    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+    const aboutSection = document.getElementById('about');
+    const offset = -75; 
+    const topPosition = aboutSection.getBoundingClientRect().top + window.pageYOffset - offset;
+
+    window.scrollTo({ top: topPosition, behavior: 'smooth' });
 }
+
+document.querySelector('nav ul li a[href="#about"]').addEventListener('click', function (event) {
+    event.preventDefault(); 
+    scrollToAbout();
+});
